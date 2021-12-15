dc93a705738043162fb6388176b24e1afa5cfa78
Ajinkya Naik <51918054+aj-naik@users.noreply.github.com>
2021-12-15 18:42:15 +0530

    background image corrected

--- src/components/home/HomePage-Component.js
+++ src/components/home/HomePage-Component.js
@@ -3,11 +3,7 @@ import './HomePage.css'
 
 export default function HomePage() {
     return (
-        <>
-        <div className='background-container'>
-        <img className='background' src='/img/homepage/background.svg' alt='backgroundimg' />
-        </div>
-        
+        <>    
         <div className='main-image'>
             <img className='main' src='/img/homepage/mainimg.svg' alt='mainimg' />
         </div>
