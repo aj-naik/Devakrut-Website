ec81edc2d48d4f132a1475ab2a1ded2d82097e31
Ajinkya Naik <51918054+aj-naik@users.noreply.github.com>
2021-12-15 18:33:15 +0530

    backgrounf image added

--- src/components/home/HomePage-Component.js
+++ src/components/home/HomePage-Component.js
@@ -3,9 +3,15 @@ import './HomePage.css'
 
 export default function HomePage() {
     return (
-        <div className='main-image'>
-            <img src='/img/homepage/mainimg.svg' alt='mainimg' />
+        <>
+        <div className='background-container'>
+        <img className='background' src='/img/homepage/background.svg' alt='backgroundimg' />
         </div>
+        
+        <div className='main-image'>
+            <img className='main' src='/img/homepage/mainimg.svg' alt='mainimg' />
+        </div>
+        </>
     )
 
 }
