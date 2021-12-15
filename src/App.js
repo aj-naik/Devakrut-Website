15278336a39bdf3fa6f4c6c0cecf2710b2d74942
Ajinkya Naik <51918054+aj-naik@users.noreply.github.com>
2021-12-15 17:02:54 +0530

    Hamburger button implemented

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import './App.css';
+import SideBar from './components/sidebar/Sidebar-component'
 
 function App() {
-  return "Hello Ajinkya from liveserver";
+  return ( <
+    SideBar pageWrapId = {
+      "page-wrap"
+    }
+    outerContainerId = {
+      "App"
+    }
+    customBurgerIcon={ <img src="img/menu/hamburger-menu.svg" /> }
+    width={ '50%' }
+    />
+    
+  );
 }
 
 export default App;
