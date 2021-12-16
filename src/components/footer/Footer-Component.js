79d35acb869ed33033e651d60e1beac42789881e
Ajinkya Naik <51918054+aj-naik@users.noreply.github.com>
2021-12-16 22:35:03 +0530

    Made Homepage and added basic footer

--- src/components/footer/Footer-Component.js
+++ src/components/footer/Footer-Component.js
@@ -4,9 +4,9 @@ import './Footer.css'
 const Footer = () => (
   <>
     <div className="footer">
-      <footer className="content">
+      <div className="content">
           <h2 className="title">Devakrut: Cold Pressed Oils!!!</h2>
-      </footer>
+      </div>
     </div>
   </>
 );
