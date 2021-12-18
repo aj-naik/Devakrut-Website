8f5ff98de939265a91d4b9fa6228857a1eb1b1bc
Ajinkya Naik <51918054+aj-naik@users.noreply.github.com>
2021-12-18 17:37:33 +0530

    Did some css stuff

--- src/components/about/AboutPage-Component.js
+++ src/components/about/AboutPage-Component.js
@@ -1,48 +1,63 @@
 import React from "react";
-import "./AboutPage.css"; 
+import "./AboutPage.css";
 import CarouselComponent from "../carousel/Carousel-Component";
+
 export default function AboutPage() {
   return (
     <div className="about-page">
       <div className="main-title">Devakrut</div>
-      <div className="text-container" id="text-1">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget
-        quisque volutpat id molestie id non morbi lorem. Augue sit elit augue
-        viverra est. A etiam mi purus arcu felis ultrices praesent tincidunt.
-        Eget a pellentesque ut urna proin adipiscing ut.
+
+      <div className="box">
+        <div className="text-container" id="text-1">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget
+          quisque volutpat id molestie id non morbi lorem. Augue sit elit augue
+          viverra est. A etiam mi purus arcu felis ultrices praesent tincidunt.
+          Eget a pellentesque ut urna proin adipiscing ut.
+        </div>
+
+        <img
+          className="image-container"
+          id="image-1"
+          src="img/about/banda1.svg"
+        />
       </div>
-      <img
-        className="image-container"
-        id="image-1"
-        src="img/about/banda1.svg"
-      />
+
       <div className="text-container" id="text-2">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget
         quisque volutpat id molestie id non morbi lorem. Augue sit elit augue
         viverra est. A etiam mi purus arcu felis ultrices praesent tincidunt.
         Eget a pellentesque ut urna proin adipiscing ut.
       </div>
+
       <img className="yellow-circle" id="circle-2" src="img/about/2nd.svg" />
-      
-      <div className="text-container" id="text-3">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget
-        quisque volutpat id molestie id non morbi lorem. Augue sit elit augue
-        viverra est. A etiam mi purus arcu felis ultrices praesent tincidunt.
-        Eget a pellentesque ut urna proin adipiscing ut.
+
+      <div className="box">
+        <div className="text-container" id="text-3">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget
+          quisque volutpat id molestie id non morbi lorem. Augue sit elit augue
+          viverra est. A etiam mi purus arcu felis ultrices praesent tincidunt.
+          Eget a pellentesque ut urna proin adipiscing ut.
+        </div>
+        
+        <img
+          className="image-container"
+          id="image-2"
+          src="img/about/banda2.svg"
+          alt="circle-1"
+        />
       </div>
+
       <img
         className="yellow-circle"
         id="circle-1"
         src="img/about/1st.svg"
         alt="circle-1"
       />
-      <img
-        className="image-container"
-        id="image-2"
-        src="img/about/banda2.svg"
-        alt="circle-1"
-      />
+
+      <div className="testimonial">
+        <div className="testimonial-text">Testimonials</div>
       <CarouselComponent />
+      </div>
     </div>
   );
 }
