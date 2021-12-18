f5b67427b12980fd9c7b05660447671fc54cd86c
Ajinkya Naik <51918054+aj-naik@users.noreply.github.com>
2021-12-18 18:33:46 +0530

    Roughly Completed About Page

--- src/components/about/AboutPage-Component.js
+++ src/components/about/AboutPage-Component.js
@@ -7,44 +7,41 @@ export default function AboutPage() {
     <div className="about-page">
       <div className="main-title">Devakrut</div>
 
-      
-        <div className="text-container-1" id="text-1">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget
-          quisque volutpat id molestie id non morbi lorem. Augue sit elit augue
-          viverra est. A etiam mi purus arcu felis ultrices praesent tincidunt.
-          Eget a pellentesque ut urna proin adipiscing ut.
-        </div>
-
-        <div className="box">
-
-        <img
-          className="image-container"
-          id="image-1"
-          src="img/about/banda1.svg"
-        />
-      
-
-      <div className="text-container-2" id="text-2">
+      <div className="text-container-1" id="text-1">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget
         quisque volutpat id molestie id non morbi lorem. Augue sit elit augue
         viverra est. A etiam mi purus arcu felis ultrices praesent tincidunt.
         Eget a pellentesque ut urna proin adipiscing ut.
       </div>
 
+      <div className="box">
+        <img
+          className="image-container"
+          id="image-1"
+          src="img/about/banda1.svg"
+          alt = "main-img-1"
+        />
+
+        <div className="text-container-2" id="text-2">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget
+          quisque volutpat id molestie id non morbi lorem. Augue sit elit augue
+          viverra est. A etiam mi purus arcu felis ultrices praesent tincidunt.
+          Eget a pellentesque ut urna proin adipiscing ut.
+        </div>
       </div>
 
-      <img className="yellow-circle" id="circle-2" src="img/about/2nd.svg" />
+      <img className="yellow-circle" id="circle-2" src="img/about/2nd.svg" alt = "circle2" />
 
       <div className="box">
-
         <img
           className="image-container"
           id="image-2"
           src="img/about/banda2.svg"
           alt="circle-1"
+          alt = "main-img-2"
         />
 
-<div className="text-container-3" id="text-3">
+        <div className="text-container-3" id="text-3">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget
           quisque volutpat id molestie id non morbi lorem. Augue sit elit augue
           viverra est. A etiam mi purus arcu felis ultrices praesent tincidunt.
@@ -61,20 +58,19 @@ export default function AboutPage() {
 
       <div className="testimonial">
         <div className="testimonial-text">Testimonials</div>
-      <CarouselComponent />
+        <CarouselComponent />
       </div>
 
       <div className="certification">
         <div className="certification-text">Certified By</div>
 
         <div className="certification-img">
-          <image src = "img/about/certified-1.svg"></image>
-          <image src = "img/about/certified-2.svg"></image>
-          <image src = "img/about/certified-3.svg"></image>
-          <image src = "img/about/certified-4.svg"></image>
+          <img src="img/about/certified.svg" alt="img1"></img>
+          {/* <img src="img/about/certified-2.svg" alt="img2"></img>
+          <img src="img/about/certified-3.svg" alt="img3"></img>
+          <img src="img/about/certified-4.svg" alt="img4"></img> */}
         </div>
       </div>
-      
     </div>
   );
 }
