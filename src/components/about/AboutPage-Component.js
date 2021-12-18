d79db6a834cbb0644c2c57dc4a18b150b7218c7d
Ajinkya Naik <51918054+aj-naik@users.noreply.github.com>
2021-12-18 17:55:04 +0530

    Changed content pattern and css

--- src/components/about/AboutPage-Component.js
+++ src/components/about/AboutPage-Component.js
@@ -7,44 +7,49 @@ export default function AboutPage() {
     <div className="about-page">
       <div className="main-title">Devakrut</div>
 
-      <div className="box">
-        <div className="text-container" id="text-1">
+      
+        <div className="text-container-1" id="text-1">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget
           quisque volutpat id molestie id non morbi lorem. Augue sit elit augue
           viverra est. A etiam mi purus arcu felis ultrices praesent tincidunt.
           Eget a pellentesque ut urna proin adipiscing ut.
         </div>
+
+        <div className="box">
 
         <img
           className="image-container"
           id="image-1"
           src="img/about/banda1.svg"
         />
-      </div>
+      
 
-      <div className="text-container" id="text-2">
+      <div className="text-container-2" id="text-2">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget
         quisque volutpat id molestie id non morbi lorem. Augue sit elit augue
         viverra est. A etiam mi purus arcu felis ultrices praesent tincidunt.
         Eget a pellentesque ut urna proin adipiscing ut.
       </div>
 
+      </div>
+
       <img className="yellow-circle" id="circle-2" src="img/about/2nd.svg" />
 
       <div className="box">
-        <div className="text-container" id="text-3">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget
-          quisque volutpat id molestie id non morbi lorem. Augue sit elit augue
-          viverra est. A etiam mi purus arcu felis ultrices praesent tincidunt.
-          Eget a pellentesque ut urna proin adipiscing ut.
-        </div>
-        
+
         <img
           className="image-container"
           id="image-2"
           src="img/about/banda2.svg"
           alt="circle-1"
         />
+
+<div className="text-container-3" id="text-3">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget
+          quisque volutpat id molestie id non morbi lorem. Augue sit elit augue
+          viverra est. A etiam mi purus arcu felis ultrices praesent tincidunt.
+          Eget a pellentesque ut urna proin adipiscing ut.
+        </div>
       </div>
 
       <img
@@ -57,6 +62,11 @@ export default function AboutPage() {
       <div className="testimonial">
         <div className="testimonial-text">Testimonials</div>
       <CarouselComponent />
+      </div>
+
+      <div className="certification">
+        <div className="certification-text">Certified By</div>
+
       </div>
     </div>
   );
