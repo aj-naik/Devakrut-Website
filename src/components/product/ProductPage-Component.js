d9183eb77a27dbba8a5ecdcc25dc834573259045
Ajinkya Naik <51918054+aj-naik@users.noreply.github.com>
2021-12-19 13:43:32 +0530

    Rearranged tabs in sidebar

--- src/components/product/ProductPage-Component.js
+++ src/components/product/ProductPage-Component.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from "react-responsive-carousel";
+// import { Carousel } from "react-responsive-carousel";
 import "./ProductPage.css";
 
 export default function ProductPage() {
