744fffe1998b71f431ed82dbe4049ff7506db923
Manan Karani <manankarani@gmail.com>
2021-12-28 21:26:29 +0530

    animations homepage part 1

--- src/components/home/HomePage-Component.js
+++ src/components/home/HomePage-Component.js
@@ -1,15 +1,50 @@
 import React from "react";
 import "./HomePage.css";
 import "./HomePage-Desktop.css";
+import "./HomePage-Anim.css";
 
 export default function HomePage() {
   return (
     <>
+    <span className="desktop l-y-cicrle"></span>
+    
       <div className="main-image-section">
         <img
           className="main-image"
           src="/img/homepage/mainimg.svg"
           alt="mainimg"
+        />
+        
+        <span className="desktop y-cicrle"></span>
+        <img
+          className="desktop bottles-desk-home scale-up-center"
+          src="/img/homepage/bottles_desk.svg"
+          alt="bottle-desk"
+        />
+        <img
+          className="desktop title-main fade-in"
+          src="/img/homepage/devakrut_title_desk.svg"
+          alt="title-desk"
+        />
+        <img
+          className="desktop nuts_home fade-in-left"
+          src="/img/homepage/groundnut_desk.svg"
+          alt="nuts-desk"
+        />
+        <img
+          className="desktop sunflower_home fade-in-bl"
+          src="/img/homepage/sunflower_desk.svg"
+          alt="suns-desk"
+        />
+        <img
+          className="desktop coconut_home fade-in-br"
+          src="/img/homepage/coconut_desk.svg"
+          alt="coconut-desk"
+        />
+        <img
+          className="desktop mustard_flower fade-in-right"
+          src="/img/homepage/mustard_flower_desk.svg"
+          alt="mustard-desk"
         />
       </div>
       <div className="process-section">
